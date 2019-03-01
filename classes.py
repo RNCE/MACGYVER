92e30538f2b034aab087018531fcde172927c1a5
RobClv <robin.clav@orange.fr>
2019-03-01 22:58:01 +0100

    add comments and written in accordance with PEP8

--- classes.py
+++ classes.py
@@ -1,175 +1,204 @@
 """
-Mac Gyver Game classes 
+Mac Gyver Game classes
 """
 
 import pygame
-from pygame.locals import *
-from constants import *
+import constants as cst
 import random
 
+
 class Labyrinth:
-	# This class allows you to create a labyrinth from a text file
-	def __init__(self, fichier):
-		self.fichier = fichier
-		self.structure = []
-		self.objects = {'needle': [], 'tube': [], 'ether': []}
+    # This class allows you to create a labyrinth from a text file
+    def __init__(self, file):
+        self.file = file
+        self.structure = []
+        self.objects = {'needle': [], 'tube': [], 'ether': []}
 
-	def create(self):
-		# Creating a list from the text file
-		# Open the file
-		with open(self.fichier, "r") as fichier:
-			structure_lab = []
-			# The lines of the file are scanned
-			for line in fichier:
-				line_lab = []
-				# The sprites of line are scanned
-				for sprite in line:
-					# "/n" are not taken into account
-					if sprite != '\n':
-						# Adding sprite to the line_lab list
-						line_lab.append(sprite)
-				# Adding lign to the structure_lab list
-				structure_lab.append(line_lab)
-			# struture_lab save
-			self.structure = structure_lab
+    def create(self):
+        # Creating a list from the text file
+        # Open the file
+        with open(self.file, "r") as file:
+            structure_lab = []
+            # The lines of the file are scanned
+            for line in file:
+                line_lab = []
+                # The sprites of line are scanned
+                for sprite in line:
+                    # "/n" are not taken into account
+                    if sprite != '\n':
+                        # Adding sprite to the line_lab list
+                        line_lab.append(sprite)
+                # Adding lign to the structure_lab list
+                structure_lab.append(line_lab)
+            # struture_lab save
+            self.structure = structure_lab
 
-	def rand_pos(self):
-		# The random number is between the number of x and y lines
-		rand_number_y = random.randint(0, NUMBER_SPRITE-1)
-		rand_number_x = random.randint(0, NUMBER_SPRITE-1)
-		pos_x = 0
-		pos_y = 0
-		# Check that the element is not a wall
-		while self.structure[rand_number_y][rand_number_x] != '0':
-			rand_number_y = random.randint(0, NUMBER_SPRITE-1)
-			rand_number_x = random.randint(0, NUMBER_SPRITE-1)
-		# Calculation of the position in pixels
-		pos_x = rand_number_x * SIZE_SPRITE
-		pos_y = rand_number_y * SIZE_SPRITE
-		position = [pos_x, pos_y]
-		return position
+    def rand_pos(self):
+        # This method allows to return a list with random positions
+        # for x and y
+        # Generate a random number between 0 and number of sprite -1
+        rand_number_y = random.randint(0, cst.NUMBER_SPRITE-1)
+        rand_number_x = random.randint(0, cst.NUMBER_SPRITE-1)
+        # Pixel position x and y
+        pos_x = 0
+        pos_y = 0
+        # Check that the element is not a wall
+        while self.structure[rand_number_y][rand_number_x] != '0':
+            rand_number_y = random.randint(0, cst.NUMBER_SPRITE-1)
+            rand_number_x = random.randint(0, cst.NUMBER_SPRITE-1)
+        # Calculation of the position in pixels
+        pos_x = rand_number_x * cst.SIZE_SPRITE
+        pos_y = rand_number_y * cst.SIZE_SPRITE
+        position = [pos_x, pos_y]
+        # return a random position that is not a wall
+        return position
 
-	def objects_pos(self):
-		avoid_overl = []
-		i = 0
-		for obj in self.objects:
-			avoid_overl.append(self.rand_pos())
-			while avoid_overl.count(avoid_overl[i]) != 1:
-				avoid_overl[i] = self.rand_pos()
-			self.objects[obj] = avoid_overl[i]
-			i += 1
+    def objects_pos(self):
+        # This method avoids the overlapping of the images
+        avoid_overl = []
+        i = 0
+        for key in self.objects:
+            # Browse the dictionary and adds all the positions in
+            # the avoid_overl list
+            avoid_overl.append(self.rand_pos())
+            # Verification of duplicate elements
+            while avoid_overl.count(avoid_overl[i]) != 1:
+                # If duplicate element, generate a new position
+                avoid_overl[i] = self.rand_pos()
+            # Add the position to the dictionary
+            self.objects[key] = avoid_overl[i]
+            i += 1
 
-	def display(self, window):
-		# This function allows to display the labyrinth according to the file
-		# Images loading
-		wall = pygame.image.load(IMAGE_GAME_WALL).convert()
-		start = pygame.image.load(IMAGE_GAME_START).convert()
-		end = pygame.image.load(IMAGE_GAME_END).convert_alpha()
-		needle = pygame.image.load(IMAGE_GAME_NEEDLE).convert()
-		tube = pygame.image.load(IMAGE_GAME_TUBE).convert()
-		ether = pygame.image.load(IMAGE_GAME_ETHER).convert()
+    def display(self, window):
+        # This function allows to display the labyrinth and the objects
+        # according to the file
+        # Images loading
+        wall = pygame.image.load(cst.IMAGE_GAME_WALL).convert()
+        start = pygame.image.load(cst.IMAGE_GAME_START).convert()
+        end = pygame.image.load(cst.IMAGE_GAME_END).convert_alpha()
+        needle = pygame.image.load(cst.IMAGE_GAME_NEEDLE).convert()
+        tube = pygame.image.load(cst.IMAGE_GAME_TUBE).convert()
+        ether = pygame.image.load(cst.IMAGE_GAME_ETHER).convert()
 
-		# Scan list
-		numb_line = 0
-		for line in self.structure:
-			# Scan lists of lines
-			numb_box = 0
-			for sprite in line:
-				# Position calculation
-				x = numb_box * SIZE_SPRITE
-				y = numb_line * SIZE_SPRITE
-				if sprite == 'w':
-					window.blit(wall, (x, y))
-				elif sprite == 's':
-					window.blit(start, (x, y))
-				elif sprite == 'e':
-					window.blit(end, (x, y))
-				numb_box += 1
-			numb_line += 1
+        # Scan list
+        numb_line = 0
+        for line in self.structure:
+            # Scan lists of lines
+            numb_box = 0
+            for sprite in line:
+                # Position calculation
+                x = numb_box * cst.SIZE_SPRITE
+                y = numb_line * cst.SIZE_SPRITE
+                if sprite == 'w':
+                    window.blit(wall, (x, y))
+                elif sprite == 's':
+                    window.blit(start, (x, y))
+                elif sprite == 'e':
+                    window.blit(end, (x, y))
+                numb_box += 1
+            numb_line += 1
 
-		# Add image of the dictionnary
-		for key in self.objects:
-			if key == 'needle':
-				window.blit(needle, (self.objects['needle'][0], self.objects['needle'][1]))
-			elif key == 'tube':
-				window.blit(tube, (self.objects['tube'][0], self.objects['tube'][1]))
-			elif key == 'ether':
-				window.blit(ether, (self.objects['ether'][0], self.objects['ether'][1]))
+        # The images from the dicionary are added to the labyrinth
+        for key in self.objects:
+            if key == 'needle':
+                window.blit(needle,
+                            (self.objects['needle'][0],
+                                self.objects['needle'][1]))
+            elif key == 'tube':
+                window.blit(tube,
+                            (self.objects['tube'][0],
+                                self.objects['tube'][1]))
+            elif key == 'ether':
+                window.blit(ether,
+                            (self.objects['ether'][0],
+                                self.objects['ether'][1]))
 
 
 class MacGyver:
-	# MacGyver class
-	def __init__(self, str_labyrinth):
-		# MacGyver image
-		self.himself = pygame.image.load(IMAGE_GAME_GYVER).convert_alpha()
-		# initial position
-		self.box_x = 0
-		self.box_y = 0
-		self.x = 0
-		self.y = 0
-		# Labyrinth structure
-		self.str_labyrinth = str_labyrinth
-		# Macgyver alives
-		self.alive = True
-		# Macgyver winner
-		self.winner = False
+    # MacGyver class
+    def __init__(self, str_labyrinth):
+        # MacGyver image
+        self.himself = pygame.image.load(cst.IMAGE_GAME_GYVER).convert_alpha()
+        # initial position
+        self.box_x = 0
+        self.box_y = 0
+        self.x = 0
+        self.y = 0
+        # Labyrinth structure
+        self.str_lab = str_labyrinth
+        # Macgyver alives
+        self.alive = True
+        # Macgyver winner
+        self.winner = False
 
-	def move(self, direction, objects):
-		# Method for changing position
-		# Up move
-		if direction == 'top':
-			# Avoids going over the scren
-			if self.box_y > 0:
-				if self.str_labyrinth.structure[self.box_y-1][self.box_x] == 'e' and not objects:
-					self.winner = True
-				elif self.str_labyrinth.structure[self.box_y-1][self.box_x] == 'e':
-					self.himself = pygame.image.load(IMAGE_GAME_GYVER_DEAD).convert()
-					self.alive = False
-				# Check that it is not a wall
-				elif self.str_labyrinth.structure[self.box_y-1][self.box_x] != 'w':
-					# One box move
-					self.box_y -= 1
-					# Convert in pixels
-					self.y = self.box_y * SIZE_SPRITE
-		# Move to the right
-		elif direction == 'right':
-			if self.box_x < (NUMBER_SPRITE - 1):
-				if self.str_labyrinth.structure[self.box_y][self.box_x+1] == 'e' and not objects:
-					self.winner = True
-				elif self.str_labyrinth.structure[self.box_y][self.box_x+1] == 'e':
-					self.himself = pygame.image.load(IMAGE_GAME_GYVER_DEAD).convert()
-					self.alive = False
-				elif self.str_labyrinth.structure[self.box_y][self.box_x+1] != 'w':
-					self.box_x += 1
-					self.x = self.box_x * SIZE_SPRITE
-		# Down move
-		elif direction == 'down':
-			if self.box_y < (NUMBER_SPRITE - 1):
-				if self.str_labyrinth.structure[self.box_y+1][self.box_x] == 'e' and not objects:
-					self.winner = True
-				elif self.str_labyrinth.structure[self.box_y+1][self.box_x] == 'e':
-					self.himself = pygame.image.load(IMAGE_GAME_GYVER_DEAD).convert()
-					self.alive = False
-				elif self.str_labyrinth.structure[self.box_y+1][self.box_x] != 'w':
-					self.box_y += 1
-					self.y = self.box_y * SIZE_SPRITE
-		# Move to the left
-		elif direction == 'left':
-			if self.box_x > 0:
-				if self.str_labyrinth.structure[self.box_y][self.box_x-1] == 'e' and not objects:
-					self.winner = True
-				elif self.str_labyrinth.structure[self.box_y][self.box_x-1] == 'e':
-					self.himself = pygame.image.load(IMAGE_GAME_GYVER_DEAD).convert()
-					self.alive = False
-				elif self.str_labyrinth.structure[self.box_y][self.box_x-1] != 'w':
-					self.box_x -= 1
-					self.x = self.box_x * SIZE_SPRITE
+    def move(self, direction, objects):
+        # Method for changing position
+        # Up move
+        if direction == 'top':
+            # Avoids going over the scren
+            if self.box_y > 0:
+                # When Mac Gyver is in front of the guard
+                # and he has all the objects
+                # not objects == objects dictionary empty
+                if (self.str_lab.structure[self.box_y-1][self.box_x] == 'e'
+                        and not objects):
+                    self.winner = True
+                # When Mac Gyver is in front of the guard
+                # but he hasn't the objects
+                elif self.str_lab.structure[self.box_y-1][self.box_x] == 'e':
+                    self.himself = pygame.image.load(cst.GYVER_DEAD).convert()
+                    self.alive = False
+                # Check that it is not a wall
+                elif self.str_lab.structure[self.box_y-1][self.box_x] != 'w':
+                    # One box move
+                    self.box_y -= 1
+                    # Convert in pixels
+                    self.y = self.box_y * cst.SIZE_SPRITE
+        # Move to the right
+        elif direction == 'right':
+            if self.box_x < (cst.NUMBER_SPRITE - 1):
+                if (self.str_lab.structure[self.box_y][self.box_x+1] == 'e'
+                        and not objects):
+                    self.winner = True
+                elif self.str_lab.structure[self.box_y][self.box_x+1] == 'e':
+                    self.himself = pygame.image.load(cst.GYVER_DEAD).convert()
+                    self.alive = False
+                elif self.str_lab.structure[self.box_y][self.box_x+1] != 'w':
+                    self.box_x += 1
+                    self.x = self.box_x * cst.SIZE_SPRITE
+        # Down move
+        elif direction == 'down':
+            if self.box_y < (cst.NUMBER_SPRITE - 1):
+                if (self.str_lab.structure[self.box_y+1][self.box_x] == 'e'
+                        and not objects):
+                    self.winner = True
+                elif self.str_lab.structure[self.box_y+1][self.box_x] == 'e':
+                    self.himself = pygame.image.load(cst.GYVER_DEAD).convert()
+                    self.alive = False
+                elif self.str_lab.structure[self.box_y+1][self.box_x] != 'w':
+                    self.box_y += 1
+                    self.y = self.box_y * cst.SIZE_SPRITE
+        # Move to the left
+        elif direction == 'left':
+            if self.box_x > 0:
+                if (self.str_lab.structure[self.box_y][self.box_x-1] == 'e'
+                        and not objects):
+                    self.winner = True
+                elif self.str_lab.structure[self.box_y][self.box_x-1] == 'e':
+                    self.himself = pygame.image.load(cst.GYVER_DEAD).convert()
+                    self.alive = False
+                elif self.str_lab.structure[self.box_y][self.box_x-1] != 'w':
+                    self.box_x -= 1
+                    self.x = self.box_x * cst.SIZE_SPRITE
 
-	def take_object(self, objects):
-		for key, values in list(objects.items()):
-			if [self.x, self.y] == values:
-				objects.pop(key)
-			elif [self.x, self.y] == values:
-				objects.pop(key)
-			elif [self.x, self.y] == values:
-				objects.pop(key)
+    def take_object(self, objects):
+        # This method allows to take the objects
+        for key, values in list(objects.items()):
+            if [self.x, self.y] == values:
+                # if the Mac Gyver position corresponds to the position of
+                # the object, the object is removed from the dictionary
+                objects.pop(key)
+            elif [self.x, self.y] == values:
+                objects.pop(key)
+            elif [self.x, self.y] == values:
+                objects.pop(key)
--- gyverlabyrinthe.py
+++ gyverlabyrinthe.py
@@ -3,139 +3,141 @@ Mac Gyver game !
 this game aims to get Mac Gyver out of the labyrinth.
 
 Script Python
-Files : gyverlabyrinthe.py, constantes.py, classes.py
+Files : gyverlabyrinthe.py, constants.py, classes.py
 Dir: macgyver_ressources
 """
 
 import pygame
-from pygame.locals import *
-
+from pygame.locals import QUIT, KEYDOWN, K_F1, K_ESCAPE, K_RIGHT, \
+                          K_LEFT, K_DOWN, K_UP
 from classes import Labyrinth, MacGyver
-from constants import *
+import constants as cst
 
 
 # main programme
 def main():
-	pygame.init()
+    pygame.init()
 
-	# Open the Pygame window (square : width=heigh)
-	window = pygame.display.set_mode((SIZE_WINDOW, SIZE_WINDOW))
-	# Icon
-	icon = pygame.image.load(IMAGE_ICON)
-	pygame.display.set_icon(icon)
-	# Title
-	pygame.display.set_caption(TITLE_WINDOW)
+    # Open the Pygame window (square : width=heigh)
+    window = pygame.display.set_mode((cst.SIZE_WINDOW, cst.SIZE_WINDOW))
+    # Icon
+    icon = pygame.image.load(cst.IMAGE_ICON)
+    pygame.display.set_icon(icon)
+    # Title
+    pygame.display.set_caption(cst.TITLE_WINDOW)
 
-	# Main loop
-	main_loop = True
-	while main_loop:
-		# Variables home, game
-		home = True
-		game = False
-		end_game = False
+    # Main loop
+    main_loop = True
+    while main_loop:
+        # Variables home, game
+        home = True
+        game = False
+        end_game = False
 
-		# Home loop
-		while home:
-			# Limitation of the loop speed
-			pygame.time.Clock().tick(30)
+        # Home loop
+        while home:
+            # Limitation of the loop speed
+            pygame.time.Clock().tick(30)
 
-			# Display and loading of the home screen
-			home = pygame.image.load(IMAGE_HOME).convert()
-			window.blit(home, (0,0))
+            # Display and loading of the home screen
+            home = pygame.image.load(cst.IMAGE_HOME).convert()
+            window.blit(home, (0, 0))
 
-			# Refreshment
-			pygame.display.flip()
+            # Refreshment
+            pygame.display.flip()
 
-			# Event management of the home page
-			for event in pygame.event.get():
-				# home to False if the user QUIT
-				if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-					home = False
-					main_loop = False
+            # Event management of the home page
+            for event in pygame.event.get():
+                # home to False if the user QUIT
+                if (event.type == QUIT or event.type == KEYDOWN
+                        and event.key == K_ESCAPE):
+                    home = False
+                    main_loop = False
 
-				elif event.type == KEYDOWN and event.key == K_F1:
-					# home to False and game to True 
-					home = False
-					game = True
+                elif event.type == KEYDOWN and event.key == K_F1:
+                    # home to False and game to True
+                    home = False
+                    game = True
 
-					# Display and loading of the background
-					game_background = pygame.image.load(IMAGE_GAME_BACKGROUND).convert()
-					window.blit(game_background, (0,0))
+                    # Display and loading of the background
+                    gm_bck = pygame.image.load(cst.IMAGE_GAME_BCKG).convert()
+                    window.blit(gm_bck, (0, 0))
 
-					# Display and loading of the labyrinth
-					labyrinth = Labyrinth(FILE_LABYRINTH)
-					labyrinth.create()
-					# Generate a random position for the objects
-					labyrinth.objects_pos()
-					# And display and loading images
-					labyrinth.display(window)					
+                    # Display and loading of the labyrinth
+                    labyrinth = Labyrinth(cst.FILE_LABYRINTH)
+                    labyrinth.create()
+                    # Generate a random position for the objects
+                    labyrinth.objects_pos()
+                    # And display and loading images
+                    labyrinth.display(window)
 
-					# Mac Gyver loading
-					macgyver = MacGyver(labyrinth)
+                    # Mac Gyver loading
+                    macgyver = MacGyver(labyrinth)
+
+        # Game loop
+        while game:
+            # Limitation of the loop speed
+            pygame.time.Clock().tick(30)
+
+            # Event management of the game screen
+            for event in pygame.event.get():
+                # If the user wants to quit the game
+                if event.type == QUIT:
+                    # Game to False and main_loop to False
+                    game = False
+                    main_loop = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        # If the user press to escape, he returns
+                        # to the main screen
+                        game = False
+                        home = True
+                    # MacGyver's movements
+                    elif event.key == K_UP:
+                        macgyver.move('top', labyrinth.objects)
+                    elif event.key == K_RIGHT:
+                        macgyver.move('right', labyrinth.objects)
+                    elif event.key == K_DOWN:
+                        macgyver.move('down', labyrinth.objects)
+                    elif event.key == K_LEFT:
+                        macgyver.move('left', labyrinth.objects)
+
+            macgyver.take_object(labyrinth.objects)
+
+            if macgyver.alive is True and macgyver.winner is True:
+                game = False
+                end_game = True
+            # Display with new positions
+            window.blit(gm_bck, (0, 0))
+            labyrinth.display(window)
+            window.blit(macgyver.himself, (macgyver.x, macgyver.y))
+
+            # Refreshment
+            pygame.display.flip()
+
+        while end_game:
+            # Limitation of the loop speed
+            pygame.time.Clock().tick(30)
+
+            # Display and loading of the end_game screen
+            home = pygame.image.load(cst.IMAGE_END).convert()
+            window.blit(home, (0, 0))
+
+            # Refreshment
+            pygame.display.flip()
+
+            # Event management of the end_game page
+            for event in pygame.event.get():
+                # main_loop to False and end_game to False if the user QUIT
+                if event.type == QUIT:
+                    end_game = False
+                    main_loop = False
+                # home to True and end_game to False if the user wants
+                # to return to the home screen
+                elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                    home = True
+                    end_game = False
 
 
-		# Game loop
-		while game:
-			# Limitation of the loop speed
-			pygame.time.Clock().tick(30)	
-
-			# Event management of the game screen
-			for event in pygame.event.get():
-				# If the user wants to quit the game
-				if event.type == QUIT:
-					# Game to False and main_loop to False
-					game = False
-					main_loop = False
-					
-				elif event.type == KEYDOWN:
-					if event.key == K_ESCAPE:
-						# If the user press to escape, he returns to the main screen
-						game = False
-						home = True
-					# MacGyver's movements
-					elif event.key == K_UP:
-						macgyver.move('top', labyrinth.objects)
-					elif event.key == K_RIGHT:
-						macgyver.move('right', labyrinth.objects)
-					elif event.key == K_DOWN:
-						macgyver.move('down', labyrinth.objects)
-					elif event.key == K_LEFT:
-						macgyver.move('left', labyrinth.objects)
-
-			macgyver.take_object(labyrinth.objects)
-
-			if macgyver.alive and macgyver.winner == True:
-				game = False
-				end_game = True
-			# Display with new positions
-			window.blit(game_background, (0,0))
-			labyrinth.display(window)
-			window.blit(macgyver.himself, (macgyver.x, macgyver.y))
-
-			# Refreshment
-			pygame.display.flip()
-
-		while end_game:
-			# Limitation of the loop speed
-			pygame.time.Clock().tick(30)
-
-			# Display and loading of the home screen
-			home = pygame.image.load(IMAGE_END).convert()
-			window.blit(home, (0,0))
-
-			# Refreshment
-			pygame.display.flip()
-
-			# Event management of the home page
-			for event in pygame.event.get():
-				# home to False if the user QUIT
-				if event.type == QUIT:
-					end_game = False
-					main_loop = False
-				elif event.type == KEYDOWN and event.key == K_ESCAPE:
-					home = True
-					end_game = False
-
-
-if __name__=="__main__":
-	main()
+if __name__ == "__main__":
+    main()
